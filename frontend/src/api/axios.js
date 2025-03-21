ef00cc9dc7022e655bbe2a44c3921d2835c3459b
R17358 <shivay.lfc@gmail.com>
2025-03-21 12:42:57 +0530

    modified urls

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api', 
+  baseURL: 'https://ai-content-creator-9fz4.onrender.com/api', 
   
 });
 
